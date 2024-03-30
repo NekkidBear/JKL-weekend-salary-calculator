b76d1d2cd3f089156942603363552d0ad02781a3
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-03-30 08:43:39 -0500

    correct ids for DOM; add variables to capture inputs

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 console.log("Javascript works!");
 
 //add function for onclick event
-function addEmployee(){
+function addEmployee(event){
+    // get field input and store it in variables
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let employeeID = document.getElementById("employeeID");
+    let jobTitle = document.getElementById("jobTitle");
+    let annualSalary = document.getElementById("annualSalary")
+    console.log("data entered: ", firstName, lastName, employeeID, jobTitle, annualSalary);
+    // select the table body
+    // add a new table row with the input
+    // update total monthly cost
+    updateMonthlyCost(salary);
+    // clear inputs
+}
+
+function updateMonthlyCost(salary){
     //...
 }
