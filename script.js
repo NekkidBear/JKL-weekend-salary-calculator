1a4930356704dc15e5e44f7ee0b4ea53c86c5474
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-03-30 09:09:54 -0500

    refactor submit

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
 //confirm javascript is sourced correctly
 console.log("Javascript works!");
 
-//add function for onclick event
-function addEmployee(event){
-    // get field input and store it in variables
-    let firstName = document.getElementById("firstName");
-    let lastName = document.getElementById("lastName");
-    let employeeID = document.getElementById("employeeID");
-    let jobTitle = document.getElementById("jobTitle");
-    let annualSalary = document.getElementById("annualSalary")
-    console.log("data entered: ", firstName, lastName, employeeID, jobTitle, annualSalary);
-    // select the table body
-    // add a new table row with the input
-    // update total monthly cost
-    updateMonthlyCost(salary);
-    // clear inputs
+/** submit takes in the form data, stores it in variables, and then creates a new
+ * table row in the DOM with the data. It calls updateMonthlyCost and passes in the annual salary data.
+ * Finally it clears the inputs so a new row can be typed.
+ */
+function submit(event) {
+  //prevent legacy default form behavior
+  event.preventDefault();
+
+  // get field input and store it in variables
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let employeeID = document.getElementById("employeeID").value;
+  let jobTitle = document.getElementById("jobTitle").value;
+  let annualSalary = document.getElementById("annualSalary").value;
+  console.log(
+    "data entered: ",
+    firstName,
+    lastName,
+    employeeID,
+    jobTitle,
+    annualSalary
+  );
+
+  // select the table body
+  let tableBody = document.getElementById("tableBody");
+
+  // add a new table row with the input
+  tableBody.innerHTML += `<tr>
+                                <td>${firstName}</td>
+                                <td>${lastName}</td>
+                                <td>${employeeID}</td>
+                                <td>${jobTitle}</td>
+                                <td>${annualSalary}</td>
+                            </tr>`;
+
+  
+  // update total monthly cost
+  updateMonthlyCost(annualSalary);
+
+  // clear inputs
+  document.getElementById("firstName").innerText = "";
+  document.getElementById("lastName").innerText = "";
+  document.getElementById("employeeID").innerText = "";
+  document.getElementById("jobTitle").innerText = "";
+  document.getElementById("annualSalary").innerText = "";
 }
 
-function updateMonthlyCost(salary){
-    //...
+function updateMonthlyCost(salary) {
+  //...
 }
